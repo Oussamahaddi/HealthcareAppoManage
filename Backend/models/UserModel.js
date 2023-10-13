88621be85dde3a7f5043dae8fce8d63d8ebab296
Oussama Haddi <oussama.turboxxx1@gmail.com>
2023-10-13 17:30:17 +0100

    add user and relation between use and client

--- Backend/models/UserModel.js
+++ Backend/models/UserModel.js
@@ -1,6 +1,7 @@
 
 import { DataTypes } from "sequelize";
 import sequelize from "../config/sequelize.js";
+import ClientModel from "./ClientModel.js";
 
 const UserModel = sequelize.define("user", {
     id: {
@@ -8,12 +9,26 @@ const UserModel = sequelize.define("user", {
         primaryKey : true,
         autoIncrement : true
     },
-    first_name : DataTypes.STRING,
+    first_name : {
+        type: DataTypes.STRING,
+        require: true,
+    },
     last_name : DataTypes.STRING,
-    email : DataTypes.STRING,
+    email : {
+        type: DataTypes.STRING,
+        require: true,
+        isEmail: true,
+    },
     password : DataTypes.STRING,
     profile_image : DataTypes.STRING,
-    role : DataTypes.ENUM("superadmin", "admin", "client", "chef", "technicien", "entreprise")
-}); 
+    role : DataTypes.ENUM("superadmin", "admin", "client", "chef", "technicien", "entreprise"),
+});
+
+
+UserModel.hasMany(ClientModel, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
+});
+ClientModel.belongsTo(UserModel);
 
 export default UserModel;
